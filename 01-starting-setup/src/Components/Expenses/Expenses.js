3498e667cf2b563f8168bbe10c30c080a9ec0ca3
lulu <luluuu0727@gmail.com>
2022-03-30 22:39:01 +1100

    update projects

--- 01-starting-setup/src/Components/Expenses/Expenses.js
+++ 01-starting-setup/src/Components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import ExpenseItem from "./ExpenseItem";
 import Card from '../UI/Card';
 import './Expenses.css';
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from './ExpensesList'
 
 
 function Expenses(props) {
@@ -16,24 +17,16 @@ function Expenses(props) {
         return expense.date.getFullYear().toString() === filteredYear;
     });
 
+
+
     return (
         <div>
             <Card className="expenses">
-                <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-
-                {filteredExpenses.length === 0 ?
-                    (<p>No expenses found.</p>) :
-
-                    (
-                        filteredExpenses.map((expense) => (
-                            <ExpenseItem
-                                key={expense.id}
-                                title={expense.title}
-                                amount={expense.amount}
-                                date={expense.date}
-                            />
-                        ))
-                    )}
+                <ExpensesFilter
+                    selected={filteredYear}
+                    onChangeFilter={filterChangeHandler}
+                />
+                <ExpensesList items={filteredExpenses} />
 
             </Card>
         </div>
