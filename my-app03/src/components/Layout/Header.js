33df2a64e3f1baf113bc7d13bd5b0c1d1116bc0e
lulu <luluuu0727@gmail.com>
2022-04-05 21:10:07 +1000

    update project03

---
+++ my-app03/src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import { Fragment } from 'react';
+
+import HeaderCartButton from './HeaderCartButton';
+
+import classes from './Header.module.css';
+
+const Header = (props) => {
+    return (
+        <Fragment>
+            <header className={classes.header}>
+                <h1>HappytMeals</h1>
+                <HeaderCartButton />
+            </header>
+            <div className={classes['main-image']}>
+                <img src={require('../Assets/meals.jpeg')} alt='A table full of delicious food!' />
+            </div>
+        </Fragment>
+    );
+};
+
+export default Header;
