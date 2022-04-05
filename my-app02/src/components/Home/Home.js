c67a06a6342b30e01324413c7ae36fb7a7320e75
lulu <luluuu0727@gmail.com>
2022-04-05 10:35:08 +1000

    update project02

--- my-app02/src/components/Home/Home.js
+++ my-app02/src/components/Home/Home.js
@@ -3,12 +3,15 @@ import Button from '../UI/Button/Button';
 
 import Card from '../UI/Card/Card';
 import classes from './Home.module.css';
+import { useContext } from 'react';
+import AuthContext from '../../store/auth-context';
 
 const Home = (props) => {
+  const authCtx = useContext(AuthContext);
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
-      <Button onClick={props.onLogout}>Logout</Button>
+      <Button onClick={authCtx.onLogout}>Logout</Button>
     </Card>
   );
 };
