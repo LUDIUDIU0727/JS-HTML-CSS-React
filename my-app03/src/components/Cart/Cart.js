7ec5fb2bcaaf11170e731457545684e0d12f7f53
lulu <luluuu0727@gmail.com>
2022-04-06 19:00:21 +1000

    update project03

--- my-app03/src/components/Cart/Cart.js
+++ my-app03/src/components/Cart/Cart.js
@@ -1,28 +1,28 @@
-import styles from './Cart.module.css'
+import Modal from '../UI/Modal';
+import classes from './Cart.module.css';
 
-const Cart = props => {
-    const cartItems = <ul className={styles['cart-items']}> {[
-        { id: 'c1', name: 'Sushi', amount: 2, price: 12.99 },
-    ].map((cartItem) => <li>{cartItem.name}</li>)}</ul>;
+const Cart = (props) => {
+    const cartItems = (
+        <ul className={classes['cart-items']}>
+            {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
+                <li>{item.name}</li>
+            ))}
+        </ul>
+    );
 
     return (
-
-        <div>
+        <Modal>
             {cartItems}
-            <div className={styles.total}>
+            <div className={classes.total}>
                 <span>Total Amount</span>
                 <span>35.62</span>
             </div>
-            <div className={styles.actions}>
-                <button className={styles['button--alt']}>Close</button>
-                <button className={styles.button}>Order</button>
-
+            <div className={classes.actions}>
+                <button className={classes['button--alt']}>Close</button>
+                <button className={classes.button}>Order</button>
             </div>
-        </div>
-    )
-
-
+        </Modal>
+    );
 };
-
 
 export default Cart;
