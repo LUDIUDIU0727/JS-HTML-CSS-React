1bd1112a62117e9cdb8a49e4523102b0790239c6
lulu <luluuu0727@gmail.com>
2022-04-11 19:37:08 +1000

    update project04

--- my-app04/src/App.js
+++ my-app04/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import Button from './components/UI/Button/Button'
 import './App.css';
+import DemoOutput from './components/Demo/DemoOutput';
 
 function App() {
+
+  const [showParagraph, setShowParagraph] = useState(false)
+
+  const showParagraphHandler = () => {
+    setShowParagraph(prevParagraph => !prevParagraph);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={showParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
 }
