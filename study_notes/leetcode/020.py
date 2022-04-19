01b7b3348ad189b93615460446efd9ed839715a7
lulu <luluuu0727@gmail.com>
2022-04-19 10:38:20 +1000

    study notes

--- study_notes/leetcode/020.py
+++ study_notes/leetcode/020.py
@@ -1,6 +1,9 @@
 """valid parentheses () [] {}"""
 
 
+from sqlalchemy import false
+
+
 class Solution(object):
     def isValid(self,s):
         stack=[]
@@ -15,3 +18,27 @@ class Solution(object):
             else:
                 return False
         return stack==[]
+
+
+def isValid(self,s:str)-> bool:
+    if len(s)==0:
+        return True
+    stack=[]
+    for c in s:
+        if c=='('or c =='[' or c =='{':
+            stack.append(c)
+        else:
+            if len(stack)==0:
+                return False
+            else:
+                temp = stack.pop()
+                if c ==')':
+                    if temp!='(':
+                        return False
+                elif c==']':
+                    if temp !='[':
+                        return False
+                elif c =='}':
+                    if temp !='{':
+                        return False
+    return True if len(stack)==0 else False
