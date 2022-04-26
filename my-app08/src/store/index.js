4d7b4f2a8879cbe2d2315ea95c55aa83adc98145
lulu <luluuu0727@gmail.com>
2022-04-26 22:58:49 +1000

    study notes

--- my-app08/src/store/index.js
+++ my-app08/src/store/index.js
@@ -1,40 +1,55 @@
-import { createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from './counter';
+import authReducer from './auth';
+
 
 //you should never change the existing state,you should return a brand new state
-const initialState = { counter: 0, showCounter: true };
+//in redux toolkit you can use state.count++(still not manipulating the existing data)
 
-const counterReducer = (state = initialState, action) => {
-    if (action.type === 'increment') {
-        return {
-            counter: state.counter + 1,
-            showCounter: state.showCounter
-        };
-    }
 
-    if (action.type === 'decrement') {
-        return {
-            counter: state.counter - 1,
-            showCounter: state.showCounter
-        };
-    }
 
-    if (action.type === 'increase') {
-        return {
-            counter: state.counter + action.amount,
-            showCounter: state.showCounter
-        };
-    }
+// const counterReducer = (state = initialState, action) => {
+//     if (action.type === 'increment') {
+//         return {
+//             counter: state.counter + 1,
+//             showCounter: state.showCounter
+//         };
+//     }
 
-    if (action.type === 'toggle') {
-        return {
-            counter: state.counter,
-            showCounter: !state.showCounter
-        };
-    }
+//     if (action.type === 'decrement') {
+//         return {
+//             counter: state.counter - 1,
+//             showCounter: state.showCounter
+//         };
+//     }
 
-    return state;
-};
+//     if (action.type === 'increase') {
+//         return {
+//             counter: state.counter + action.amount,
+//             showCounter: state.showCounter
+//         };
+//     }
 
-const store = createStore(counterReducer);
+//     if (action.type === 'toggle') {
+//         return {
+//             counter: state.counter,
+//             showCounter: !state.showCounter
+//         };
+//     }
+
+//     return state;
+// };
+
+// const store = createStore(counterReducer);
+
+
+//----------------------------------------------------------------------------------
+//counterSlice.actions.toggleCounter() returns an action object of this shape:
+//{ type:'some auto-generated unique identifier'}
+
+const store = configureStore({
+    reducer: { counter: counterReducer, auth: authReducer }
+});
+
 
 export default store;
