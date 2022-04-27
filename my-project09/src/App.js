2c573546d9cd12cd8893bd6971a1e04524953f63
lulu <luluuu0727@gmail.com>
2022-04-27 17:47:46 +1000

    project09

--- my-project09/src/App.js
+++ my-project09/src/App.js
@@ -1,55 +1,34 @@
+import { Fragment, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
-import { uiActions } from './store/ui-slice';
-import { Fragment } from 'react';
 import Notification from './components/UI/Notification';
-
+import { sendCartData, fetchCartData } from './store/cart-actions';
 
 let isInitial = true;
-function App() {
-  const showCart = useSelector(state => state.ui.cartIsVisible);
-  const cart = useSelector(state => state.cart);
-  const dispatch = useDispatch();
-  const notification = useSelector(state => state.ui.notification);
-  useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(uiActions.showNotification({
-        status: 'pending',
-        title: 'sending...',
-        message: 'sending cart data!',
-      }))
-      const response = await fetch('https://react-htttp-24e60-default-rtdb.firebaseio.com/cart.json', {
-        method: 'PUT',
-        body: JSON.stringify(cart),
-      });
-      if (!response.ok) {
-        throw new Error('sending cart data failed.');
-      }
 
-      dispatch(uiActions.showNotification({
-        status: 'success',
-        title: 'success!',
-        message: 'sending cart data successfully!',
-      }))
-    };
+function App() {
+  const dispatch = useDispatch();
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
+
+  useEffect(() => {
     if (isInitial) {
       isInitial = false;
       return;
     }
 
-    sendCartData().catch(error => {
-      dispatch(uiActions.showNotification({
-        status: 'error',
-        title: 'error!',
-        message: 'sending cart data failed!',
-      }))
-
-    })
-
-  }, [cart, dispatch])   //this effect reexecuted whenever our cart changes
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
+  }, [cart, dispatch]);
 
   return (
     <Fragment>
--- my-project09/src/store/cart-slice.js
+++ my-project09/src/store/cart-slice.js
@@ -1,18 +1,22 @@
-import { createSlice } from "@reduxjs/toolkit";
-
+import { createSlice } from '@reduxjs/toolkit';
 
 const cartSlice = createSlice({
     name: 'cart',
     initialState: {
         items: [],
         totalQuantity: 0,
-        totalAmount: 0,
+        changed: false,
     },
     reducers: {
+        replaceCart(state, action) {
+            state.totalQuantity = action.payload.totalQuantity;
+            state.items = action.payload.items;
+        },
         addItemToCart(state, action) {
             const newItem = action.payload;
-            const existingItem = state.items.find(item => item.id === newItem.id)
+            const existingItem = state.items.find((item) => item.id === newItem.id);
             state.totalQuantity++;
+            state.changed = true;
             if (!existingItem) {
                 state.items.push({
                     id: newItem.id,
@@ -21,31 +25,26 @@ const cartSlice = createSlice({
                     totalPrice: newItem.price,
                     name: newItem.title,
                 });
-
-            }
-            else {
+            } else {
                 existingItem.quantity++;
-                existingItem.totalPrice += newItem.price;
+                existingItem.totalPrice = existingItem.totalPrice + newItem.price;
             }
         },
         removeItemFromCart(state, action) {
             const id = action.payload;
-            const existingItem = state.items.find(item => item.id === id);
+            const existingItem = state.items.find((item) => item.id === id);
             state.totalQuantity--;
+            state.changed = true;
             if (existingItem.quantity === 1) {
-                state.items = state.items.filter(item => item.id !== existingItem.id);
-            }
-            else {
+                state.items = state.items.filter((item) => item.id !== id);
+            } else {
                 existingItem.quantity--;
                 existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
             }
-
         },
-
-
     },
 });
 
-
 export const cartActions = cartSlice.actions;
+
 export default cartSlice;
