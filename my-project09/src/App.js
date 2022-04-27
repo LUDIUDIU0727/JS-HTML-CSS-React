71ba1daf3a39fef7f008f491aa4a504e7211d98a
lulu <luluuu0727@gmail.com>
2022-04-27 16:32:45 +1000

    project09

--- my-project09/src/App.js
+++ my-project09/src/App.js
@@ -1,14 +1,70 @@
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { uiActions } from './store/ui-slice';
+import { Fragment } from 'react';
+import Notification from './components/UI/Notification';
+
+
+let isInitial = true;
 function App() {
   const showCart = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart);
+  const dispatch = useDispatch();
+  const notification = useSelector(state => state.ui.notification);
+  useEffect(() => {
+    const sendCartData = async () => {
+      dispatch(uiActions.showNotification({
+        status: 'pending',
+        title: 'sending...',
+        message: 'sending cart data!',
+      }))
+      const response = await fetch('https://react-htttp-24e60-default-rtdb.firebaseio.com/cart.json', {
+        method: 'PUT',
+        body: JSON.stringify(cart),
+      });
+      if (!response.ok) {
+        throw new Error('sending cart data failed.');
+      }
+
+      dispatch(uiActions.showNotification({
+        status: 'success',
+        title: 'success!',
+        message: 'sending cart data successfully!',
+      }))
+    };
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch(error => {
+      dispatch(uiActions.showNotification({
+        status: 'error',
+        title: 'error!',
+        message: 'sending cart data failed!',
+      }))
+
+    })
+
+  }, [cart, dispatch])   //this effect reexecuted whenever our cart changes
+
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
--- my-project09/src/index.js
+++ my-project09/src/index.js
@@ -7,3 +7,11 @@ import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Provider store={store}><App /></Provider>);
+
+
+// for synchronous,side-effect free code(such as data transformations), 
+// prefer reducers, avoid put logic code in action creators or components.
+
+
+// for async or code with side-effects,
+// prefer action creators or components.never use reducers.
