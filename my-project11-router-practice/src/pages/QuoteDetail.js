063217b3486b1e0755f664ec4925e4cdb342da5c
lulu <luluuu0727@gmail.com>
2022-05-02 12:44:56 +1000

    project10-router

--- my-project11-router-practice/src/pages/QuoteDetail.js
+++ my-project11-router-practice/src/pages/QuoteDetail.js
@@ -1,23 +1,26 @@
-import { Route, Link, useRouteMatch } from "react-router-dom";
-import { useParams } from "react-router-dom";
-import Comments from '../components/comments/Comments';
+import { Fragment, useEffect } from 'react';
+import { useParams, Route, Link, useRouteMatch } from 'react-router-dom';
+
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
-import useHttp from "../hooks/use-http";
-import { getSingleQuote } from "../lib/api";
-import { useEffect } from "react";
+import Comments from '../components/comments/Comments';
+import useHttp from '../hooks/use-http';
+import { getSingleQuote } from '../lib/api';
 import LoadingSpinner from '../components/UI/LoadingSpinner';
-
-
 
 const QuoteDetail = () => {
     const match = useRouteMatch();
     const params = useParams();
+
     const { quoteId } = params;
-    const { senRequest, status, data: loadedQuote, error } = useHttp(getSingleQuote, true);
+
+    const { sendRequest, status, data: loadedQuote, error } = useHttp(
+        getSingleQuote,
+        true
+    );
 
     useEffect(() => {
-        senRequest(quoteId);
-    }, [senRequest, quoteId])
+        sendRequest(quoteId);
+    }, [sendRequest, quoteId]);
 
     if (status === 'pending') {
         return (
@@ -28,37 +31,28 @@ const QuoteDetail = () => {
     }
 
     if (error) {
-        return (
-            <p className='centered'>{error}</p>
-
-        );
+        return <p className='centered'>{error}</p>;
     }
 
     if (!loadedQuote.text) {
-        return <p>NO Quote found!</p>
-
+        return <p>No quote found!</p>;
     }
 
     return (
-        <section>
+        <Fragment>
             <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
             <Route path={match.path} exact>
-                <div className="centered">
-                    <Link className="btn--flat" to={`${match.url}/comments`}>
-                        Loaded Comments
+                <div className='centered'>
+                    <Link className='btn--flat' to={`${match.url}/comments`}>
+                        Load Comments
                     </Link>
                 </div>
             </Route>
-
             <Route path={`${match.path}/comments`}>
                 <Comments />
             </Route>
-
-        </section>
-
-
-    )
-
+        </Fragment>
+    );
 };
 
 export default QuoteDetail;
